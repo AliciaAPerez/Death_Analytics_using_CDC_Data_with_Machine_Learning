b585502f6d2171ddc26640299e28ce6dd2eca0af
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-04-14 21:11:40 -0500

    death data

--- py/app.py
+++ py/app.py
@@ -5,11 +5,13 @@ import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 import os
 from test2 import getdata
+
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
 # def config():
     #engine = create_engine(
         #'postgres://xyapznezeaqswq:563af6f1a293c52937083ed16940c84d274b616f8e2202c9cd74c95ae39916b8@ec2-52-21-252-142.compute-1.amazonaws.com:5432/d4g7j0al3kor5t')
+
 @app.route("/test")   
 def test(): 
     return getdata()

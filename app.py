e3399a56e72833000c6a7f751953858fc4c93e7e
Claude-Hanfou <claudehanfou@gmail.comgit config --global user.name Claude-Hanfougit config --global user.email you@example.com  git config --global user.name Your>
2021-04-17 14:10:14 -0500

    image folder

--- app.py
+++ app.py
@@ -103,25 +103,23 @@ def machine():
 
 
 
+# @app.route('/static/<path:path>')
+# def send_js(path):
+#     return send_from_directory('static', path)
 
 
-
-@app.route('/static/<path:path>')
-def send_js(path):
-    return send_from_directory('static', path)
-
-
-@app.after_request
-def add_header(r):
-    """
-    Add headers to both force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
-    """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
-    r.headers["Pragma"] = "no-cache"
-    r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
-    return r
+# @app.after_request
+# def add_header(r):
+#     """
+#     Add headers to both force latest IE rendering engine or Chrome Frame,
+#     and also to cache the rendered page for 10 minutes.
+#     """
+#     r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+#     r.headers["Pragma"] = "no-cache"
+#     r.headers["Expires"] = "0"
+#     r.headers['Cache-Control'] = 'public, max-age=0'
+#     return 
+    
 if (__name__) == '__main__':
     app.debug = True
     app.run()

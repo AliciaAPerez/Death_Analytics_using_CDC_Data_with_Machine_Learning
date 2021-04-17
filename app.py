cdb13943c39bd0475e62c9382a4b8042a8cc0dd2
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-04-17 07:53:37 -0500

    updating

--- app.py
+++ app.py
@@ -75,6 +75,49 @@ def add_header(r):
     r.headers['Cache-Control'] = 'public, max-age=0'
     return r
 
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/index2")
+def index2():
+    return render_template("index2.html")
+
+@app.route("/index3")
+def index3():
+    return render_template("index3.html")
+
+@app.route("/index4")
+def index4():
+    return render_template("index4.html")
+
+@app.route("/index5")
+def index5():
+    return render_template("index5.html")
+
+@app.route("/index6")
+def index6():
+    return render_template("index6.html")
+
+@app.route("/index7")
+def index7():
+    return render_template("index7.html")
+
+@app.route("/index8")
+def index8():
+    return render_template("index8.html")
+
+@app.route("/index9")
+def index9():
+    return render_template("index9.html")
+
+@app.route("/index10")
+def index10():
+    return render_template("index10.html")
+
+@app.route("/deathmachine")
+def machine():
+    return render_template("deathmachine.html")
 
 
 if (__name__) == '__main__':

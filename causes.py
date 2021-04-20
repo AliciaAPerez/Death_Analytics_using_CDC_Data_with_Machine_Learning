7a117771cd3acbd01ef55138bc63b3facb1e0346
AliciaAPerez <72763332+AliciaAPerez@users.noreply.github.com>
2021-04-20 15:05:44 -0500

    updating

---
+++ causes.py
@@ -0,0 +1,81 @@
+from keras.models import load_model
+
+# returns a compiled model
+# identical to the previous one
+death_model = load_model('death_machine_keras10.h5')
+
+
+# ### Import Libraries
+
+# import pandas as pd
+# import numpy as np
+# import matplotlib.pyplot as plt
+# import seaborn as sns
+# import warnings
+# import pickle
+
+# warnings.filterwarnings('ignore')
+
+# ### Import Datset
+# df = pd.read_csv("dataset.csv")
+
+
+
+# ### Splitting Data
+
+# X = df[['age', 'trestbps', 'chol', 'thalach', 'oldpeak']]
+# y = df['target']
+
+# from sklearn.model_selection import train_test_split
+
+# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size= 0.33, random_state=0)
+
+# #### Data Preprocessing
+
+# from sklearn.preprocessing import StandardScaler
+
+# scaler = StandardScaler()
+
+# X_train = scaler.fit_transform(X_train)
+# X_test = scaler.transform(X_test)
+
+
+
+# from sklearn.neighbors import KNeighborsClassifier
+# train_scores = []
+# test_scores = []
+# for k in range(1, 20, 2):
+#     knn = KNeighborsClassifier(n_neighbors=k)
+#     knn.fit(X_train, y_train)
+#     train_score = knn.score(X_train, y_train)
+#     test_score = knn.score(X_test, y_test)
+#     train_scores.append(train_score)
+#     test_scores.append(test_score)
+#     print(f"k: {k}, Train/Test Score: {train_score:.3f}/{test_score:.3f}")
+
+
+
+# # from sklearn.ensemble import RandomForestClassifier
+# # from sklearn.metrics import classification_report 
+# # rf = RandomForestClassifier()
+# # rf = rf.fit(X_train, y_train)
+    
+# predictions = knn.predict(X_test)
+# predictions
+
+
+
+
+# from sklearn.metrics import classification_report
+# from sklearn.metrics import confusion_matrix
+
+# print("Confusion Matrix : \n\n" , confusion_matrix(predictions,y_test))
+
+# print("Classification Report : \n\n" , classification_report(predictions,y_test),"\n")
+
+
+# pickle.dump(knn, open('model.pkl', 'wb'))
+# pickle.dump(scaler, open('scaler.pkl', 'wb'))
+
+# model = pickle.load(open('model.pkl', 'rb'))
+# print(model)
